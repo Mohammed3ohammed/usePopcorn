be0e554b7b759ab79587478016798685e9052fd7
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-10 07:13:59 +0200

    Fix: corrected props naming, typo in className, and improved function naming

--- src/components/App-v1.js.jsx
+++ src/components/App-v1.js.jsx
@@ -65,6 +65,10 @@ const tempMovieData = [
 
         <Main>
             <Box><MovieList movies={movies} /></Box>
+            <Box>
+                <WatchedSummary watched={watched} />
+                <WatchedMoviesList watched={watched} />
+            </Box>
         </Main>
         </>
     )   
@@ -93,7 +97,7 @@ const tempMovieData = [
 
             return (
                 <input 
-                className="serch" type="text"  placeholder="search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />
+                className="search" type="text"  placeholder="search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />
 
             )
         }
@@ -126,7 +130,7 @@ const tempMovieData = [
             return (
                 <ul className="list">
                     {movies?.map((movie) => (
-                        <Movie movies={movie} key={movie.imdbID} />
+                        <Movie movie={movie} key={movie.imdbID} />
                     ))}
                 </ul>
             )
@@ -140,6 +144,69 @@ const tempMovieData = [
                         <p>
                             <span>🗓</span>
                             <span>{movie.Year}</span>
+                        </p>
+                    </div>
+                </li>
+            )
+        }
+
+        function WatchedSummary({ watched }) {
+            const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+            const avgUserRating = average(watched.map((movie) => movie.userRating));
+            const avgRuntime = average(watched.map((movie) => movie.runtime));
+            
+            return (
+                <div className="summary">
+                    <h2>Movies you watched</h2>
+                    <div>
+                        <p>
+                        <span>#️⃣</span>
+                        <span>{watched.length} movies</span>
+                        </p>
+                        <p>
+                            <span>⭐️</span>
+                            <span>{avgImdbRating}</span>
+                        </p>
+                        <p>
+                            <span>⭐️</span>
+                            <span>{avgUserRating}</span>
+                        </p>
+                        <p>
+                            <span>⏳</span>
+                            <span>{avgRuntime}</span>
+                        </p>
+                    </div>
+                </div>
+            )
+        }
+
+        function WatchedMoviesList({ watched }) {
+            return (
+                <ul className="list">
+                    {watched.map((movie) => (
+                        <WatchedMovie movie={movie} key={movie.imdbID} />
+                    ))}
+                </ul>
+            )
+        }
+
+        function WatchedMovie({ movie }) {
+            return( 
+                <li>
+                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                    <h3>{movie.Title}</h3>
+                    <div>
+                        <p>
+                            <span>⭐️</span>
+                            <span>{movie.imdbRating}</span>
+                        </p>
+                        <p>
+                            <span>⭐️</span>
+                            <span>{movie.userRating}</span>
+                        </p>
+                        <p>
+                            <span>⏳</span>
+                            <span>{movie.runtime}</span>
                         </p>
                     </div>
                 </li>
