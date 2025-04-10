a49e39b09a6cf011d8463dee6c335971576da532
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-10 09:14:55 +0200

    Fix bugs and clean up code for movie app components

---
+++ src/components/App-v2.jsx
@@ -0,0 +1,163 @@
+import {useEffect, useState} from"react";
+import StartRatin from"./Start/StarRating";
+
+
+const tempMovieData = [
+    {
+      imdbID: "tt1375666",
+      Title: "Inception",
+      Year: "2010",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt0133093",
+      Title: "The Matrix",
+      Year: "1999",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+      imdbID: "tt6751668",
+      Title: "Parasite",
+      Year: "2019",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+  ];
+  
+  const tempWatchedData = [
+    {
+      imdbID: "tt1375666",
+      Title: "Inception",
+      Year: "2010",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+      runtime: 148,
+      imdbRating: 8.8,
+      userRating: 10,
+    },
+    {
+      imdbID: "tt0088763",
+      Title: "Back to the Future",
+      Year: "1985",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+      runtime: 116,
+      imdbRating: 8.5,
+      userRating: 9,
+    },
+  ];
+
+  const avarage = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+  const KEY = "f84fc31d";
+
+  export default function  App() {
+    const [query, setQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [watched, setWatched] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
+    const [selectedId, setSelectedId] = useState(null);
+
+    return (
+        <>
+        <NavBar>
+            <Search query={query} setQuery={setQuery} />
+            <NumResults movies={movies} />
+        </NavBar>
+        </>
+    )
+  }
+
+
+  function Loarder() {
+    return (
+        <p className="loader">Loading....</p>
+    )
+  }
+
+  function ErrorMessage({ message }) {
+    return(
+        <p className="error">
+            <span>⛔️</span>
+        </p>
+    )
+  }
+
+  function NavBar( {children} ) {
+    return (
+        <nav className="nav-bar">
+            <Logo />
+            {children}
+        </nav>
+    )
+  }
+
+  function Logo() {
+    return(
+        <div className="logo">
+            <span role="img">🍿</span>
+            <h1>usePopcorn</h1>
+        </div>
+    )
+  }
+
+  function Search( {query, setQuery} ) {
+    return (
+        <input 
+        className="search" type="text" placeholder="search movies ..."
+         value={query} onChange={(e) => setQuery(e.target.value)} />
+    )
+  }
+
+  function NumResults({ movies }) {
+    return (
+        <p className="num-results">
+            Found <stron>{movies.length}</stron> results
+        </p>
+    )
+  }
+
+  function Main({ children }) {
+    return <main className="main">{children}</main>
+  }
+
+  function Box( {children} )  {
+    const [isOpen, setIsOpen] = useState(true);
+
+    return (
+        <div className="box">
+            <button className="btn-toggle" onClick={() => setIsOpen((open) =>!open)}>
+                {isOpen ? "-" : "+"}
+            </button>
+            {isOpen && children}
+        </div>
+    )
+  }
+
+  function MovieList({ movies, onSelectMovie }) {
+    return (
+        <ul class  Name="list list-movies">
+            {movies?.map((movie) => (
+                <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} /> 
+            ))}
+        </ul>
+    )
+  }
+
+  function Movie( {movie, onSelectMovie} ) {
+    return (
+        <li onClick={() => onSelectMovie(movie.imdbID)}>
+            <img src={movie.Poster} alt={`${movie.title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>🗓</span>
+                    <span>{movie.Year}</span>
+                </p>
+            </div>
+        </li>
+    )
+  }
