b7189369f96b1f33bf6045d7d5524db9ef079e5b
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-11 09:19:42 +0200

    Initial commit with movie app code

--- src/components/App-v2.jsx
+++ src/components/App-v2.jsx
@@ -182,7 +182,7 @@ const tempMovieData = [
         Director: director,
         Genre: genre,
     } = movie;
-  }
+
 function handleAdd() {
     const newWatchedMovie = {
         imdbID: selectedId,
@@ -196,3 +196,105 @@ function handleAdd() {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+     function callback(e) {
+      if(e.code === "Escape") {
+        onCloseMovie();
+      }
+     }
+
+    document.addEventListener("keydown", callback);
+    
+    return function() {
+      document.removeEventListener("keydown", callback);
+    
+  
+  }
+  }
+    [onCloseMovie]
+  )
+
+  useEffect(
+    function () {
+      async function getMovieDetails() {
+        setIsLoading(true);
+        const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+        );
+        const data = await res.json();
+        setMovie(data);
+        setIsLoading(false);
+      }
+      getMovieDetails();
+    },
+    [selectedId]
+  );
+
+  useEffect(
+    function () {
+      if(!title) return;
+      document.title = `movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      }
+    },
+    [title]
+  );
+
+  return (
+    <div className="details">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+        <header>
+          <button className="btn-back" onClick={onCloseMovie}>
+            &larr;
+          </button>
+          <img src={poster} alt={`Poster of ${movie} movie`} />
+          <div className="details-overview">
+            <h2>{title}</h2>
+            <p>
+              {released} &bull; {runtime}
+            </p>
+            <p>{genre}</p>
+            <p>
+              <span>⭐️</span>
+              {imdbRating} IMDb rating
+            </p>
+          </div>
+        </header>
+        <section>
+          <div className="rating">
+            {!isWatched ? (
+              <>
+              <StartRatin maxRating={110} 
+              size={24} onSetRating={setUserRating}
+              />
+              {userRating > 0 && (
+                <button className="btn-add" onClick={handleAdd}>+ Add to list</button>
+              )}
+              </>
+            ) : (
+              <p>
+                You reated with movie {watchedUserRating} <span>⭐️</span>
+              </p>
+            )}
+          </div>
+          <p>
+            <em>{plot}</em>
+          </p>
+          <p>Strring {actors}</p>
+          <p>Directed by {director}</p>
+        </section>
+        </>
+      )
+      }
+
+    </div>
+
+  );
+}
