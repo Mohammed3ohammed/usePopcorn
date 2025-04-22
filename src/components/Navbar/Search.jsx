239260ea56fc485f3aaabc8246af2b70c7b001e0
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-04-22 18:58:31 +0200

    feat: auto-focus input on Enter key if not already focused

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default  function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useKey("Enter", function () {
+    if(document.activeElement === inputEl.current) return;
+    inputEl.current.focus();
+    setQuery("");
+  });
 
     return (
       <input
